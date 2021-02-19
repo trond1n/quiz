454705b68ae9bb1f9b6facb2ca7d025d7bfbbcaf
Yuri Trondin <lopata@2ch.hk>
2021-02-19 10:26:07 +0500

    сделал роуты

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
+import { Route, Switch } from "react-router-dom";
+import Auth from "./containers/Auth/Auth";
 import Quiz from "./containers/Quiz/Quiz";
+import QuizCreator from "./containers/QuizCreator/QuizCreator";
+import QuizList from "./containers/QuizList/QuizList";
 import Layout from "./hoc/Layout/Layout";
 
 function App() {
   return (
     <Layout>
-      <Quiz />
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/quiz-creator" component={QuizCreator} />
+        <Route path="/quiz/:id" component={Quiz} />
+        <Route exact path="/" component={QuizList} />
+      </Switch>
     </Layout>
   );
 }
