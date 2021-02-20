a61732996a3cc0820d8c481cb12d4da09705b900
Yuri Trondin <lopata@2ch.hk>
2021-02-20 14:44:48 +0500

    редакс страницы списка

--- src/containers/QuizList/QuizList.jsx
+++ src/containers/QuizList/QuizList.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 import { NavLink } from "react-router-dom";
-import axios from "../../axios/axios-quiz";
 import Loader from "../../components/Loader/Loader";
+import { fetchQuizes } from "../../store/actions/quiz";
 import classes from "./QuizList.module.css";
 
-export default class QuizList extends Component {
-  state = { quizes: [], loading: true };
+class QuizList extends Component {
   renderQuizes() {
-    return this.state.quizes.map((quiz) => {
+    return this.props.quizes.map((quiz) => {
       return (
         <li key={quiz.id}>
           <NavLink to={"/quiz/" + quiz.id}>{quiz.name}</NavLink>
@@ -16,25 +16,9 @@ export default class QuizList extends Component {
     });
   }
 
-  async componentDidMount() {
-    // axios.get('https://quizwhoizz-default-rtdb.firebaseio.com/quiz.json').then(response=>{
-    //   console.log(response)
-    // })
-
-    try {
-      const response = await axios.get("/quizes.json");
-      const quizes = [];
-      Object.keys(response.data).forEach((key, index) => {
-        quizes.push({ id: key, name: `Test #${index + 1}` });
-      });
-
-      this.setState({
-        quizes,
-        loading: false,
-      });
-    } catch (error) {
-      console.log(error);
-    }
+  componentDidMount() {
+    this.props.fetchQuizes();
+   
   }
 
   render() {
@@ -42,9 +26,26 @@ export default class QuizList extends Component {
       <div className={classes.QuizList}>
         <div>
           <h1>Список тестов</h1>
-          {this.state.loading ? <Loader /> : <ul>{this.renderQuizes()}</ul>}
+          {this.props.loading && this.props.quizes.length !== 0 ? (
+            <Loader />
+          ) : (
+            <ul>{this.renderQuizes()}</ul>
+          )}
         </div>
       </div>
     );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    quizes: state.quiz.quizes,
+    loading: state.quiz.loading,
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    fetchQuizes: () => dispatch(fetchQuizes()),
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(QuizList);
---
+++ src/store/reducers/rootReducer.jsx
@@ -0,0 +1,10 @@
+import { combineReducers } from "redux"
+import quizReducer from "./quiz"
+
+
+export default combineReducers({
+quiz: quizReducer
+
+
+
+})
